993a708304cd05d51a19893530f4ff46ec4ce8b1
Caio Brainer <92734524+CaioBrainer@users.noreply.github.com>
2023-04-09 21:40:06 -0300

    Add files via upload

--- Amphibole.py
+++ Amphibole.py
@@ -9,46 +9,57 @@ import pandas as pd
 
 # Anfibólio
 
-def normalization_pt1(dataframe):
-    """Anions proportion calculation"""
+def normalization(epma_table):
+    """Anions proportion calculation
 
-    df_anion = pd.DataFrame()
+                :param epma_table: Amphibole EPMA tabular data.
+                :returns: Anion and Cation normalized proportions dataframes
+    """
 
-    df_anion['Si_anion'] = (dataframe['SiO2'] * 2) / 60.0843
-    df_anion['Ti_anion'] = (dataframe['TiO2'] * 2) / 79.8788
-    df_anion['Al_anion'] = (dataframe['Al2O3'] * 3) / 101.9602
-    df_anion['Cr_anion'] = (dataframe['Cr2O3'] * 3) / 151.9902
-    df_anion['Fe3_anion'] = (dataframe['Fe2O3'] * 3) / 60.0843
-    df_anion['Fe2_anion'] = dataframe['FeO'] / 71.8464
-    df_anion['Mn_anion'] = dataframe['MnO'] / 70.9374
-    df_anion['Mg_anion'] = dataframe['MgO'] / 40.3044
-    df_anion['Ca_anion'] = dataframe['CaO'] / 56.0794
-    df_anion['Na_anion'] = dataframe['Na2O'] / 61.9774
-    df_anion['K_anion'] = dataframe['K2O'] / 94.196
-    df_anion['total_anion'] = df_anion.sum(axis='columns')
+    def normalization_anion():
+        """Anions proportion calculation
+            :returns: Anion normalized proportions epma_table
+        """
+        df_anion = pd.DataFrame()
 
-    return df_anion
+        df_anion['Si_anion'] = (epma_table['SiO2'] * 2) / 60.0843
+        df_anion['Ti_anion'] = (epma_table['TiO2'] * 2) / 79.8788
+        df_anion['Al_anion'] = (epma_table['Al2O3'] * 3) / 101.9602
+        df_anion['Cr_anion'] = (epma_table['Cr2O3'] * 3) / 151.9902
+        df_anion['Fe3_anion'] = (epma_table['Fe2O3'] * 3) / 60.0843
+        df_anion['Fe2_anion'] = epma_table['FeO'] / 71.8464
+        df_anion['Mn_anion'] = epma_table['MnO'] / 70.9374
+        df_anion['Mg_anion'] = epma_table['MgO'] / 40.3044
+        df_anion['Ca_anion'] = epma_table['CaO'] / 56.0794
+        df_anion['Na_anion'] = epma_table['Na2O'] / 61.9774
+        df_anion['K_anion'] = epma_table['K2O'] / 94.196
+        df_anion['total_anion'] = df_anion.sum(axis='columns')
 
+        return df_anion
 
-def normalization_pt2(dataframe):
-    """Cation proportion calculation"""
-    df_cation = pd.DataFrame()
+    def normalization_cation():
+        """Cation proportion calculation
+            :returns: Cation normalized proportions epma_table
+        """
+        df_cation = pd.DataFrame()
 
-    df_cation['Si_cation'] = dataframe['SiO2'] / 60.0843
-    df_cation['Ti_cation'] = dataframe['TiO2'] / 79.8788
-    df_cation['Al_cation'] = (dataframe['Al2O3'] * 2) / 101.9602
-    df_cation['Cr_cation'] = (dataframe['Cr2O3'] * 2) / 151.9902
-    df_cation['Fe3_cation'] = (dataframe['Fe2O3'] * 2) / 60.0843
-    df_cation['Fe2_cation'] = dataframe['FeO'] / 71.8464
-    df_cation['Mn_cation'] = dataframe['MnO'] / 70.9374
-    df_cation['Mg_cation'] = dataframe['MgO'] / 40.3044
-    df_cation['Ca_cation'] = dataframe['CaO'] / 56.0794
-    df_cation['Na_cation'] = (dataframe['Na2O'] * 2) / 61.9774
-    df_cation['K_cation'] = (dataframe['K2O'] * 2) / 94.196
-    df_cation['total_cation'] = df_cation.sum(axis='columns') - df_cation['Ca_cation'] - df_cation['Sr_cation']
-    df_cation['total_cation + Ca + Sr'] = df_cation.sum(axis='columns')  # falta adicionar o Sr
+        df_cation['Si_cation'] = epma_table['SiO2'] / 60.0843
+        df_cation['Ti_cation'] = epma_table['TiO2'] / 79.8788
+        df_cation['Al_cation'] = (epma_table['Al2O3'] * 2) / 101.9602
+        df_cation['Cr_cation'] = (epma_table['Cr2O3'] * 2) / 151.9902
+        df_cation['Fe3+_cation'] = (epma_table['Fe2O3'] * 2) / 60.0843
+        df_cation['Fe2+_cation'] = epma_table['FeO'] / 71.8464
+        df_cation['Mn_cation'] = epma_table['MnO'] / 70.9374
+        df_cation['Mg_cation'] = epma_table['MgO'] / 40.3044
+        df_cation['Ca_cation'] = epma_table['CaO'] / 56.0794
+        df_cation['Na_cation'] = (epma_table['Na2O'] * 2) / 61.9774
+        df_cation['K_cation'] = (epma_table['K2O'] * 2) / 94.196
+        df_cation['total_cation'] = df_cation.sum(axis='columns') - df_cation['Ca_cation'] - df_cation['Sr_cation']
+        df_cation['total_cation + Ca + Sr'] = df_cation.sum(axis='columns')  # falta adicionar o Sr
 
-    return df_cation
+        return df_cation
+
+    return normalization_anion(), normalization_cation()
 
 
 def normalization_pt3a(dataframe):
@@ -58,9 +69,8 @@ def normalization_pt3a(dataframe):
     df_s13['Si_S13'] = 13 * dataframe['Si_anion'] / dataframe['total_cation']
     df_s13['Ti_S13'] = 13 * dataframe['Ti_anion'] / dataframe['total_cation']
     df_s13['Al_S13'] = 13 * dataframe['Al_anion'] / dataframe['total_cation']
-    df_s13['Cr_S13'] = 13 * dataframe['Cr_anion'] / dataframe['total_cation']
-    df_s13['Fe3_S13'] = 13 * dataframe['Fe3_anion'] / dataframe['total_cation']
-    df_s13['Fe2_S13'] = 13 * dataframe['Fe2_anion'] / dataframe['total_cation']
+    df_s13['Fe3+_S13'] = 13 * dataframe['Fe3_anion'] / dataframe['total_cation']
+    df_s13['Fe2+_S13'] = 13 * dataframe['Fe2_anion'] / dataframe['total_cation']
     df_s13['Mn_S13'] = 13 * dataframe['Mn_anion'] / dataframe['total_cation']
     df_s13['Mg_S13'] = 13 * dataframe['Mg_anion'] / dataframe['total_cation']
     df_s13['Ca_S13'] = 13 * dataframe['Ca_anion'] / dataframe['total_cation']
@@ -80,8 +90,8 @@ def normalization_pt3b(dataframe):
     df_s15['Ti_S15'] = 15 * dataframe['Ti_anion'] / dataframe['total_cation + Ca + Sr']
     df_s15['Al_S15'] = 15 * dataframe['Al_anion'] / dataframe['total_cation + Ca + Sr']
     df_s15['Cr_S15'] = 15 * dataframe['Cr_anion'] / dataframe['total_cation + Ca + Sr']
-    df_s15['Fe3_S15'] = 15 * dataframe['Fe3_anion'] / dataframe['total_cation + Ca + Sr']
-    df_s15['Fe2_S15'] = 15 * dataframe['Fe2_anion'] / dataframe['total_cation + Ca + Sr']
+    df_s15['Fe3+_S15'] = 15 * dataframe['Fe3_anion'] / dataframe['total_cation + Ca + Sr']
+    df_s15['Fe2+_S15'] = 15 * dataframe['Fe2_anion'] / dataframe['total_cation + Ca + Sr']
     df_s15['Mn_S15'] = 15 * dataframe['Mn_anion'] / dataframe['total_cation + Ca + Sr']
     df_s15['Mg_S15'] = 15 * dataframe['Mg_anion'] / dataframe['total_cation + Ca + Sr']
     df_s15['Ca_S15'] = 15 * dataframe['Ca_anion'] / dataframe['total_cation + Ca + Sr']
@@ -92,6 +102,53 @@ def normalization_pt3b(dataframe):
     return df_s15
 
 
-'''def iron_state():
-    s13_fe3 = 2*F-total anions
-    se s13_fe3 < 0 = 0, se não s13_fe3 = s13_fe3 = 2*F-s13total anions'''
+def iron_ox_state(dataframe):
+    """
+    Iron oxidation state calculation
+    """
+
+    # Cálculo da oxidação para S13
+    fe3 = 2 * (23-dataframe['total_S13'])
+    if fe3 < 0:
+        fe3 = 0
+    else:
+        fe3 = fe3
+
+    if fe3 > dataframe['Fe2+_S13']:
+        fe3 = dataframe['Fe2+_S13']
+    else:
+        fe3 = fe3
+
+    fe2 = dataframe['Fe2+_S13'] - fe3
+
+    # Cálculo da oxidação para S15
+    fe3 = 2 * (23 - dataframe['total_S15'])
+    if fe3 < 0:
+        fe3 = 0
+    else:
+        fe3 = fe3
+
+    if fe3 > dataframe['Fe2+_S15']:
+        fe3 = dataframe['Fe2+_S15']
+    else:
+        fe3 = fe3
+
+    fe2 = dataframe['Fe2+_S15'] - fe3
+
+
+def cation_o23_s13(dataframe):
+    cat_df = pd.DataFrame()
+
+    cat_df['Si'] = 23 * dataframe['Si_cation'] / dataframe['total_anion']
+    cat_df['Ti'] = 23 * dataframe['Ti_cation'] / dataframe['total_anion']
+    cat_df['Al'] = 23 * dataframe['Al_cation'] / dataframe['total_anion']
+    cat_df['Fe3+'] = 23 * dataframe['Fe3+_S13'] * (dataframe['total_cation'] / 13) / dataframe['total_anion']
+    cat_df['Fe2+'] = 23 * dataframe['Fe2+_S13'] * (dataframe['total_cation'] / 13) / dataframe['total_anion']
+    cat_df['Mn'] = 23 * dataframe['Si_cation'] / dataframe['total_anion']
+    cat_df['Mg'] = 23 * dataframe['Ti_cation'] / dataframe['total_anion']
+    cat_df['Ca'] = 23 * dataframe['Al_cation'] / dataframe['total_anion']
+    cat_df['Na'] = 23 * dataframe['Si_cation'] / dataframe['total_anion']
+    cat_df['K'] = 23 * dataframe['Ti_cation'] / dataframe['total_anion']
+    cat_df['Total'] = cat_df.sum(axis='columns')
+
+    return cat_df
